39a15687443ffac40f59b2b13751b56557937c4a
batrinu <batrinu@outlook.com>
2025-08-07 13:06:29 +0300

    fix build

--- src/admin.tsx
+++
@@ -1,7 +0,0 @@
-import CMS from 'decap-cms-app'
-
-// Initialize the CMS object
-CMS.init()
-
-// Optional: Register custom widgets or editor plugins here
-// CMS.registerWidget('customWidget', CustomWidgetComponent)
